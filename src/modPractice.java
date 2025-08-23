e8aefa897957928742f034fbd4cb4fda0bc0df24
powerlock <michael.guo002@gmail.com>
2025-08-23 08:49:07 -0700

    unit 1

---
+++ src/modPractice.java
@@ -0,0 +1,20 @@
+// % or the mod operator is a mathematical  method to get remainder of a calculation.
+/** One flower costs $7 but a dozen flower cost $70. Finish the program where the user enter
+ * the number of flowers, and the program displays the cost. You will need to use the mod operator.
+ * Example 1:
+ * How many flowers?
+ * 5
+ * 5 flowers cost $35
+ *  * Example 2:
+ *  * How many flowers?
+ *  * 27
+ *  * 27 flowers cost $161
+ */
+
+public class modPractice {
+    public static void main(String[] args)
+    {
+        // print out the result of 34%5, is this result the same as your calculation? -- use mental math
+
+    }
+}
