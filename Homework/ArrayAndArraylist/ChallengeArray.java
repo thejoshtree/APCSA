6b2bb55eedd5e0a093e4fa0d2c2e83a010037daf
thejoshtree <67811548+thejoshtree@users.noreply.github.com>
2026-02-04 21:26:46 -0800

    Update ChallengeArray.java

--- Homework/ArrayAndArraylist/ChallengeArray.java
+++ Homework/ArrayAndArraylist/ChallengeArray.java
@@ -31,3 +31,39 @@ public class ChallengeArray
         // Answer:
     }
 }
+
+/// MY CODE IS FROM HERE
+/// ---------------------------------------
+/// i used chatgpt a little bit on removing item 0
+
+import java.util.*;
+
+public class ChallengeArray
+{
+    public static void main(String[] args)
+    {
+        // Using an ArrayList instead of an array
+        ArrayList String toDoList = new ArrayList String[];
+
+        toDoList.add("Do homework");
+        toDoList.add("Help make dinner");
+        toDoList.add("Call grandma");
+
+        // changing element 1
+        toDoList.set(1, "Order pizza");
+
+        System.out.println(toDoList.length + " things to do!");
+        System.out.println("Here's the first thing to do: " + toDoList[0]);
+
+        // remove item 0
+        toDoList.remove(0);
+
+        System.out.println("Here's the next thing to do: " + toDoList[0]);
+
+    // Why is an ArrayList better than an array for a toDoList?
+// Answer: An ArrayList is better because you can easily add and remove tasks,
+// it automatically resizes, and you don't have to manually shift items like you
+// would with an array.
+    }
+}
+
